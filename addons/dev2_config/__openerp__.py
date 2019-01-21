2d7e93deb183660f65192be3bc3155fe542682bb
Michael Karrer <michaelkarrer81@gmail.com>
2019-01-21 12:52:23 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/dev2_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'dev2_config',
+    'summary': '''FS-Online dev2 instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance dev2
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
